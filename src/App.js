49012899d07bf0d3f420be2945289fcb0ad35e77
Gabriel Maso <gmaso@exisoft.com.ar>
2020-11-10 02:29:48 -0300

    First commit, work in progress

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import logo from './assets/p1.png';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App" >
+            <header className="App-header" >
+                <img src={logo}
+                    className="App-logo"
+                    alt="logo" />
+                <p><code> Pokedex in progress </code></p>
+                <a className="App-link"
+                    href="https://pokeapi.co/"
+                    target="_blank"
+                    rel="noopener noreferrer" >
+                    PokeApi </a>
+            </header>
+            <footer className="App-footer">
+                <h4>Code by Gabriel Maso</h4>
+            </footer>
+        </div>
+    );
 }
 
 export default App;
